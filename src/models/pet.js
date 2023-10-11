d35ac800a3308db0f76f4450cd87999fb24fc8cd
Santi Caprile <santiagocaprile7@gmail.com>
2023-10-11 19:20:39 -0300

    Changes of timestamps management in pet model

--- src/models/pet.js
+++ src/models/pet.js
@@ -1,48 +1,56 @@
 const mongoose = require("mongoose");
 
-const petSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 100,
-  },
-  species: {
-    type: String,
-    required: true,
-    enum: ["dog", "cat", "other"],
-  },
-  breed: {
-    type: String,
-    required: true,
-  },
-  age: {
-    number: Number,
-    unit: {
+const petSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      enum: ["days", "weeks", "months", "years"],
+      required: true,
+      maxlength: 100,
     },
-  },
-  description: {
-    type: String,
-    maxlength: 400,
-  },
-  photos: [String],
-  status: {
-    type: String,
-    enum: ["available", "adopted", "inProgress"],
-    default: "available",
-  },
-  createdAt: { type: Date, default: Date.now },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  characteristics: [
-    {
-      key: String,
-      value: {
-        type: mongoose.Schema.Types.Mixed,
+    species: {
+      type: String,
+      required: true,
+      enum: ["dog", "cat", "other"],
+    },
+    breed: {
+      type: String,
+      required: true,
+    },
+    age: {
+      number: Number,
+      unit: {
+        type: String,
+        enum: ["days", "weeks", "months", "years"],
       },
     },
-  ],
-});
+    description: {
+      type: String,
+      maxlength: 400,
+    },
+    photos: [String],
+    status: {
+      type: String,
+      enum: ["available", "adopted", "inProgress"],
+      default: "available",
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    characteristics: [
+      {
+        key: String,
+        value: {
+          type: mongoose.Schema.Types.Mixed,
+        },
+      },
+    ],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Pet = mongoose.model("Pet", petSchema);
 
