d576b3ea0ab39e78fe7baa9e8f2a4c7888ea0a3e
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-07 12:50:50 -0300

    refactor: Update Rescuer controller to handle user's profilepic

--- src/controllers/rescuerController.js
+++ src/controllers/rescuerController.js
@@ -1,4 +1,5 @@
 import Rescuer from "../models/rescuer.js";
+import User from "../models/user.js";
 import cloudinary from '../config/cloudinaryConfig.js'
 
 export async function createRescuer(req, res) {
@@ -30,7 +31,8 @@ export async function createRescuer(req, res) {
 
 export async function getRescuers(req, res) {
 	try {
-		const rescuers = await Rescuer.find().populate("user", "name email");
+		const rescuers = await Rescuer.find()
+			.populate("user", "name email");
 		res.status(200).json({ rescuers });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
@@ -42,7 +44,7 @@ export async function getRescuerById(req, res) {
 		//this will find by user id in the rescuer model
 		const { id } = req.params;
 		const rescuer = await Rescuer.findOne({ user: id })
-			.populate("user", "name email")
+			.populate("user", "name email profilePic")
 		res.status(200).json({ rescuer });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
@@ -53,38 +55,42 @@ export async function updateRescuer(req, res) {
 	try {
 		const { id } = req.params;
 		const {
-			contactPhone,//
-			socialMediasLinks,//
-			//profilePic,//
-			city,//
-			bio,//
+			contactPhone,
+			socialMediasLinks,
+			profilePic,
+			city,
+			bio,
 		} = req.body;
 		console.log(req.body);
-
-		// let uploadImgUrl
-		// try {
-		// 	const uploadResponse = await cloudinary.v2.uploader
-		// 		.upload(image, {
-		// 			folder: 'profilePics',
-		// 			resource_type: 'image',
-		// 			allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'avif'],
-		// 		});
-		// 	uploadImgUrl = uploadResponse.secure_url;
-		// } catch (error) {
-		// 	console.log(error);
-		// 	return res.status(500).json({ message: error.message });
-		// }
 
 		const rescuer = await Rescuer.updateOne(
 			{ user: id },
 			{
 				contactPhone,
 				socialMediasLinks,
-				//profilePic: uploadImgUrl,
 				city,
 				bio,
 			}
 		);
+		if (profilePic) {
+			let uploadImgUrl
+			try {
+				const uploadResponse = await cloudinary.v2.uploader
+					.upload(profilePic, {
+						folder: 'profilePics',
+						resource_type: 'image',
+						allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'avif'],
+					});
+				uploadImgUrl = uploadResponse.secure_url;
+			} catch (error) {
+				console.log(error);
+				return res.status(500).json({ message: error.message });
+			}
+			await User.updateOne(
+				{ _id: id },
+				{ profilePic: uploadImgUrl, }
+			);
+		}
 		res.status(200).json({ rescuer });
 	} catch (error) {
 		console.log(error);
