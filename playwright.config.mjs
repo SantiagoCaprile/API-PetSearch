e317e3f0a4b7f1ffce77fa20292b5135d2bfcb0d
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-09 22:46:42 -0300

    refactor: Update GitHub workflow to upload test results

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -10,7 +10,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['junit', { outputFile: 'test-results/results.xml' }],
+  ],
   use: {
     trace: 'on-first-retry',
   },
