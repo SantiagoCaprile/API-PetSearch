2723db620579751239843ac3eafa7d5ccfda19a3
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-10 18:59:12 -0300

    refactor: Add API key validation middleware to rescuers routes

--- src/routes/rescuers.js
+++ src/routes/rescuers.js
@@ -8,23 +8,25 @@ import {
 	addPetToRescuer,
 	removePetFromRescuer,
 } from "../controllers/rescuerController.js";
+import { authenticateToken } from '../middlewares/authMiddleware.js';
+import { validateApiKey } from "../middlewares/apiKeyMiddleware.js";
 
 // Ruta para crear un nuevo rescatista
-router.post("/rescuers", createRescuer);
+router.post("/rescuers", validateApiKey, createRescuer);
 
 // Ruta para obtener la lista de rescatistas
-router.get("/rescuers", getRescuers);
+router.get("/rescuers", validateApiKey, getRescuers);
 
 // Ruta para obtener un rescatista por id
-router.get("/rescuers/:id", getRescuerById);
+router.get("/rescuers/:id", validateApiKey, getRescuerById);
 
 // Ruta para actualizar un rescatista
-router.put("/rescuers/:id", updateRescuer);
+router.put("/rescuers/:id", validateApiKey, authenticateToken, updateRescuer);
 
 // Ruta para agregar una mascota a un rescatista
-router.post("/rescuers/:id/pets", addPetToRescuer);
+router.post("/rescuers/:id/pets", validateApiKey, authenticateToken, addPetToRescuer);
 
 // Ruta para eliminar una mascota de un rescatista
-router.delete("/rescuers/:id/pets", removePetFromRescuer);
+router.delete("/rescuers/:id/pets", validateApiKey, authenticateToken, removePetFromRescuer);
 
 export default router;
