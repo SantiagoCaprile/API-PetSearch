d946c78a451798ff1daf20ba7ea92180d93c599c
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-10 19:30:09 -0300

    feat: Add API key validation middleware to pets routes

--- src/routes/pets.js
+++ src/routes/pets.js
@@ -9,20 +9,21 @@ import {
 } from "../controllers/petController.js";
 import { authorizeRole } from "../middlewares/roleAuthMiddleware.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
+import { validateApiKey } from "../middlewares/apiKeyMiddleware.js";
 
 // Ruta para crear una nueva mascota
 router.post("/pets", authorizeRole("admin", "rescuer"), authenticateToken, createPet);
 
 // Ruta para obtener la lista de mascotas
-router.get("/pets", getAllPets);
+router.get("/pets", validateApiKey, getAllPets);
 
 // Ruta para obtener las mascotas de un rescatista
-router.get("/pets/rescuer/:rescuerId", getRescuerPets);
+router.get("/pets/rescuer/:rescuerId", validateApiKey, getRescuerPets);
 
-// Ruta para obtener 10 mascotas aleatorias
-router.get("/random-pets", getRandomAnimals);
+// Ruta para obtener 4 mascotas aleatorias
+router.get("/pets/random", validateApiKey, getRandomAnimals);
 
 // Ruta para obtener una mascota por id
-router.get("/pets/:petId", getPetById);
+router.get("/pets/:petId", validateApiKey, getPetById);
 
 export default router;
