8f7bb6dfeb4019a7cc29063addad99cabe805899
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-10 05:09:00 -0300

    chore: Add tests for authentication middleware

---
+++ tests/middleware-tests/authMiddleware.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+// const { createServer } = require('http');
+// import app from '../src/app';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const protectedRoute = 'http://localhost:4000/users/';
+// let server;
+
+// test.beforeAll(() => {
+//     server = createServer(app);
+//     server.listen(3000);
+// });
+
+// test.afterAll(() => {
+//     server.close();
+// });
+
+test('should allow access with valid token', async ({ request }) => {
+    const token = jwt.sign({ id: 1, username: 'testuser' }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+    const response = await request.get(protectedRoute, {
+        headers: {
+            'authorization': 'Bearer ' + token,
+        },
+    });
+
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toBeInstanceOf(Array);
+});
+
+test('should deny access without token', async ({ request }) => {
+    const response = await request.get(protectedRoute);
+    expect(response.status()).toBe(401);
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty('message', 'No token provided');
+});
+
+test('should deny access with invalid token', async ({ request }) => {
+    const token = 'invalid.token';
+    const response = await request.get(protectedRoute, {
+        headers: {
+            authorization: 'Bearer ' + token,
+        },
+    });
+    expect(response.status()).toBe(403);
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty('message', 'Invalid token');
+});
