24c4529e0ce91f728f60f5d5c7d8f1efe15d13ba
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-24 23:12:40 -0300

    refactor: Documentation improved in auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,6 +4,14 @@ import jwt from 'jsonwebtoken';
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
+/**
+ * Generate a token with the user data
+ * @param {Object} user - The user object containing the necessary data
+ * @param {string} user._id - The user's ID
+ * @param {string} user.email - The user's email
+ * @param {string} user.role - The user's role
+ * @returns {string} - The generated token
+ */
 export function generateToken(user) {
     return jwt.sign(
         { id: user._id, email: user.email, role: user.role },
@@ -13,6 +21,11 @@ export function generateToken(user) {
     );
 }
 
+/**
+ * Verify if the token is valid
+ * @param {string} token - The token to verify
+ * @returns {Object} - The decoded token
+ */
 export function verifyToken(token) {
     try {
         return jwt.verify(token, secretKey);
